d5130ab638ce55b34e855f781b8c36d3f75184b9
Judah Almanzor <154279144+BrwnStallion@users.noreply.github.com>
2024-02-01 14:16:20 -0800

    style: add grid creation comments

---
+++ script.js
@@ -0,0 +1,29 @@
+// --------------------------- Etch-A-Sketch -----------------------------------
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Pseudocode ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// Create 16 x 16 grid of square divs
+    // Declare array const to store all grid div elements
+    // Create div elements cyclically
+        // Store each div element at the next position in the array const
+        // Add class attribute for each grid div
+        // Append the newly created div to grid container div
+    // OR: append the divs to the grid container div cyclically through the
+        // array
+        // This method may be preferred because we aren't creating and rewriting
+        // over the same variable as we append. This method allows for one array
+        // to be defined, and then we can append all of its members
+    // Can also add the class attribute by toggling the attribute for each/all
+
+
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Functions ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Execution ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
